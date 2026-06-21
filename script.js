56232717b4bcc4f6b017189e5e82ce134bb28771
harireddy75 <harireddy9231@gmail.com>
2026-06-22 02:28:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,63 @@
 
+let cart = JSON.parse(localStorage.getItem("cart")) || {};
+let totalPrice = Number(localStorage.getItem("totalPrice")) || 0;
+
 let cartCount = 0;
-let totalPrice = 0;
 
-let cart = {};
+for(let product in cart){
+    cartCount += cart[product].quantity;
+}
 
-function addToCart(productName, price) {
+updateCartCount();
+
+function updateCartCount(){
+    let countElement = document.getElementById("cart-count");
+
+    if(countElement){
+        countElement.innerText = cartCount;
+    }
+}
+
+function addToCart(productName, price){
 
     cartCount++;
     totalPrice += price;
 
-    document.getElementById("cart-count").innerText = cartCount;
-    document.getElementById("total-price").innerText = totalPrice;
-
-    if (cart[productName]) {
+    if(cart[productName]){
         cart[productName].quantity++;
-    } else {
+    }
+    else{
         cart[productName] = {
             price: price,
             quantity: 1
         };
     }
 
-    renderCart();
     localStorage.setItem("cart", JSON.stringify(cart));
-localStorage.setItem("totalPrice", totalPrice);
+    localStorage.setItem("totalPrice", totalPrice);
+
+    updateCartCount();
+
+    alert(productName + " added to cart!");
 }
 
-function renderCart() {
-
-    let cartItems = document.getElementById("cart-items");
-
-    cartItems.innerHTML = "";
-
-    for (let product in cart) {
-
-        let li = document.createElement("li");
-
-        li.innerHTML =
-            product +
-            " | Qty: " +
-            cart[product].quantity +
-            " | ₹" +
-            (cart[product].price * cart[product].quantity);
-
-        cartItems.appendChild(li);
-    }
-}
-
-function searchProducts() {
+function searchProducts(){
 
     let input = document.getElementById("search").value.toLowerCase();
 
     let cards = document.getElementsByClassName("card");
 
-    for (let i = 0; i < cards.length; i++) {
+    for(let i=0;i<cards.length;i++){
 
         let title = cards[i]
-            .getElementsByTagName("h3")[0]
-            .innerText
-            .toLowerCase();
+        .getElementsByTagName("h3")[0]
+        .innerText
+        .toLowerCase();
 
-        if (title.includes(input)) {
+        if(title.includes(input)){
             cards[i].style.display = "block";
-        } else {
+        }
+        else{
             cards[i].style.display = "none";
         }
     }
